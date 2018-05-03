416f2c1b36400811bdf8f42737dc1b6ce7ed5e31
yolan cristoria <yolan.cristoria@hotmail.com>
2018-05-03 15:17:16 +0200

    Initial Commit

---
+++ Werkstuk1_Yolan_Cristoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werkstuk1_Yolan_Cristoria
+//
+//  Created by yolan cristoria on 03/05/2018.
+//  Copyright © 2018 yolan cristoria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
